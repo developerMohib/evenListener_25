60437b1a9c87e61cd6f3e459a99259ed08722738
Mohib <mohibullahmohim2020@gmail.com>
2024-02-13 16:47:27 +0600

    event listener add .

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// 2nd styte to call funtion
 
 function defaultBg(){
     document.body.style.backgroundColor = 'white';
@@ -7,7 +8,30 @@ function makeGreen(){
     document.body.style.backgroundColor = 'green';
 }
 
+// 3rd system to call fuction 
+
 let makeRed = document.getElementById('makeRed');
     makeRed.onclick = function(){
     document.body.style.backgroundColor = 'red';
 }
+// 3rd another system
+
+let makeBlue = document.getElementById('makeBlue');
+makeBlue.onclick = make_Blue ;
+function make_Blue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+// 4rth system to call function
+
+let makeYellow = document.getElementById('yellow');
+makeYellow.addEventListener('click', makeYollow);
+
+function makeYollow(){
+    document.body.style.backgroundColor = 'yellow';
+}
+let makePink = document.getElementById('pink');
+makePink.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'pink';
+});
+
